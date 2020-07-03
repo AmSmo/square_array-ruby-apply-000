470a42a1fd462420a27a24b8c4ad31cb6973331d
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-03 21:43:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
-  new_array = []
-  numbers.each do |num|
-    new_array.push(num ** 2)
-  end
-  new_array
+  # new_array = []
+  # numbers.each do |num|
+  #   new_array.push(num ** 2)
+  # end
+  # new_array
+  numbers.collect
 end
