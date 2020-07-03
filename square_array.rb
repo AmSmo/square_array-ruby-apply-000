824e4c5fb6afb07b2c594c63dc1836cc07c9355c
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-03 21:42:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
 def square_array(numbers)
-  count=0
+  # count=0
+  # numbers.each do |num|
+  #   numbers[count] = num ** 2
+  #   count+=1
+  # end
+  # numbers
+  new_array = []
   numbers.each do |num|
-    numbers[count] = num ** 2
-    count+=1
+    new_array.push(num ** 2)
   end
-  numbers
+  new_array
 end
